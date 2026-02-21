2b87c98591016ef25c1f21901156acf8ee166be4
muhaimanalishah <miloasdev@gmail.com>
2026-02-21 21:44:29 +0500

    format codebase

--- lib/types/supabase.types.ts
+++ lib/types/supabase.types.ts
@@ -73,6 +73,30 @@ export type Database = {
           },
         ];
       };
+      users: {
+        Row: {
+          created_at: string | null;
+          email: string;
+          id: string;
+          name: string;
+          updated_at: string | null;
+        };
+        Insert: {
+          created_at?: string | null;
+          email: string;
+          id: string;
+          name: string;
+          updated_at?: string | null;
+        };
+        Update: {
+          created_at?: string | null;
+          email?: string;
+          id?: string;
+          name?: string;
+          updated_at?: string | null;
+        };
+        Relationships: [];
+      };
     };
     Views: {
       [_ in never]: never;
