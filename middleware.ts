3c599838704d362c3d18713e678da0040ca04e7b
muhaimanalishah <miloasdev@gmail.com>
2026-02-21 19:57:36 +0500

    format code

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    console.log('Middleware executed for:', request.nextUrl.pathname);
-    return NextResponse.next();
+  console.log('Middleware executed for:', request.nextUrl.pathname);
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/admin/:path*"],
-}
+  matcher: ['/admin/:path*'],
+};
